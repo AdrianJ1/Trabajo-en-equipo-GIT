8b4e3bb0ca9c9769063ed8896e85eea115287293
Alvaro Gonzalez Chamorro <agoncha733a@ieslacampina.es>
2021-10-14 10:24:57 +0200

    INSERT CURSO3

--- INSERT.sql
+++ INSERT.sql
@@ -1,3 +1,5 @@
 INSERT INTO USUARIO1 (usuario) VALUES ('Pepe');
 INSERT INTO AULA2 (Nombre_aula, capacidad, planta, descripción)
 VALUES ('ASIR', 25, 2, 'Aula con ordenadores');
+INSERT INTO CURSO3 (Alumno, Codigo, Clave, Descripcion, Duracion)
+VALUES ('Paquito Perez', 25, 'Paco2000', 'Curso de informatica', 300);
