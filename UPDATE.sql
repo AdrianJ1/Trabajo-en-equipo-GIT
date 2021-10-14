70beb0cedb0e4d021454c8c39178d3f0925bec19
Adrian Jesus Morillas Abadia <amoraba141a@ieslacampina.es>
2021-10-14 10:03:10 +0200

    Actualizar tablas sql

--- UPDATE.sql
+++ UPDATE.sql
@@ -1,1 +1,7 @@
-UPDATE
+update table 
+set Nombre = Carlos
+set Apellidos = Rodrigo Aguilar
+set Edad = 20
+set Dirección = Calle Falsa 123
+set Nº de hermanos = 0
+set Correo electronico del alumno = estenoesuncorreofalso@gmail.com;
