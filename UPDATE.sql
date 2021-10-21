5ebb9930cc84e7e3b9e424fca6b2987ff19f9ae7
Adrian Jesus Morillas Abadia <amoraba141a@ieslacampina.es>
2021-10-21 09:25:28 +0200

    Arreglo final del sql

--- UPDATE.sql
+++ UPDATE.sql
@@ -1,7 +1,24 @@
-update table 
-set Nombre like Carlos
-set Apellidos like Rodrigo Aguilar
-set Edad = 20
-set Dirección like Calle Falsa 123
-set Nº de hermanos = 0
+update table   USUARIO1
+set Nombre like Carlos,
+set Apellidos like Rodrigo Aguilar,
+set Edad = 20,
+set Dirección like Calle Falsa 123,
+set Nº de hermanos = 0,
 set Correo electronico del alumno like estenoesuncorreofalso@gmail.com;
+
+update table   AULA2
+  SET  NOMBRE_AULA LIKE ASIR_1,
+  SET  CAPACIDAD = 20,
+  SET  PLANTA =1,
+  SET  DESCRIPCION LIKE GRADO SUPERIOR;
+
+update table   CURSO3
+  SET  ALUMNO LIKE GILLERMO MONTERO GONZALEZ,
+  SET CODIGO = 1420,
+  SET  CLAVE =347809,
+  SET DESCRIPCION LIKE ALUMNO ESTANDAR,
+  SET  HORAS_DURACION = 6;
+
+update table  EDIFICIO4
+ SET   NOMBRE LIKE DEPARTAMENTO INFORMATICA ,
+ SET DESCRIPCION LIKE EDIFICIO INFORMATICA;
